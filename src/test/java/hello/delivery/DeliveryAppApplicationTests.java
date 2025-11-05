5a3e31c05e1a78e9849b3e3ec6941da3312d83b0
wooseop <useop0821@gmail.com>
2025-11-05 17:30:47 +0900

    initialize deliveryApp project

---
+++ src/test/java/hello/delivery/DeliveryAppApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.delivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
