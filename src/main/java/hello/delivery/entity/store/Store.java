a89119177e31d73bb4b42c2162c874e5b4e1b050
wooseop <useop0821@gmail.com>
2025-11-06 13:15:38 +0900

    feat: store 엔티티 구현

---
+++ src/main/java/hello/delivery/entity/store/Store.java
@@ -0,0 +1,45 @@
+package hello.delivery.entity.store;
+
+import hello.delivery.entity.BaseEntity;
+import hello.delivery.entity.product.Product;
+import hello.delivery.entity.user.Owner;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Store extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Owner owner;
+
+    private String name;
+
+    private int totalSales;
+
+    @Enumerated(EnumType.STRING)
+    private StoreType storeType;
+
+    @OneToMany(mappedBy = "store", cascade = CascadeType.ALL, orphanRemoval = true)
+    private final List<Product> products = new ArrayList<>();
+
+    @Builder
+    private Store(Owner owner, String name, StoreType storeType) {
+        this.owner = owner;
+        this.name = name;
+        this.storeType = storeType;
+        this.totalSales = 0;
+    }
+
+}
