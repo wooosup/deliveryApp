98c9609f2afb6bddeeb0efaf1b0f41c94ccf4727
wooseop <useop0821@gmail.com>
2025-11-09 19:35:53 +0900

    feat: jpa dip 적용

--- src/main/java/hello/delivery/owner/infrastructure/OwnerJpaRepository.java
+++ src/main/java/hello/delivery/owner/infrastructure/OwnerJpaRepository.java
@@ -2,6 +2,6 @@ package hello.delivery.owner.infrastructure;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface OwnerRepository extends JpaRepository<Owner, Long> {
+public interface OwnerJpaRepository extends JpaRepository<OwnerEntity, Long> {
 
 }
---
+++ src/main/java/hello/delivery/owner/service/port/OwnerRepository.java
@@ -0,0 +1,11 @@
+package hello.delivery.owner.service.port;
+
+import hello.delivery.owner.domain.Owner;
+import java.util.Optional;
+
+public interface OwnerRepository {
+
+    Owner save(Owner owner);
+
+    Optional<Owner> findById(Long id);
+}
