de46d52eae9e5d0f5470cc2d3702d33520867cf8
wooseop <useop0821@gmail.com>
2025-11-09 22:57:27 +0900

    feat: 상태변경 매서드 추가 구현

--- src/main/java/hello/delivery/product/infrastructure/ProductSellingStatus.java
+++ src/main/java/hello/delivery/product/infrastructure/ProductSellingStatus.java
@@ -1,11 +1,20 @@
 package hello.delivery.product.infrastructure;
 
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
+@Getter
 @RequiredArgsConstructor
 public enum ProductSellingStatus {
     SELLING("판매중"),
     STOP_SELLING("판매중지");
 
     private final String text;
+
+    public ProductSellingStatus changeStatus(ProductSellingStatus status) {
+        if (this == status) {
+            throw new IllegalArgumentException("이미 " + status.text + " 상태입니다.");
+        }
+        return status;
+    }
 }
