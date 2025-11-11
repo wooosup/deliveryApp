aa219e2920deceb8ab586b88e4fa056b6ece32da
wooseop <useop0821@gmail.com>
2025-11-11 19:16:31 +0900

    test: fake user 리포지토리 구현

---
+++ src/test/java/hello/delivery/mock/FakeUserRepository.java
@@ -0,0 +1,47 @@
+package hello.delivery.mock;
+
+import hello.delivery.user.domain.User;
+import hello.delivery.user.service.port.UserRepository;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.atomic.AtomicLong;
+
+public class FakeUserRepository implements UserRepository {
+
+    private final AtomicLong autoIncrement = new AtomicLong(1);
+    private final List<User> data = new ArrayList<>();
+
+    @Override
+    public User save(User user) {
+        if (user.getId() == null) {
+            User newUser = User.builder()
+                    .id(autoIncrement.getAndIncrement())
+                    .name(user.getName())
+                    .username(user.getUsername())
+                    .password(user.getPassword())
+                    .address(user.getAddress())
+                    .build();
+            data.add(newUser);
+            return newUser;
+        } else {
+            data.removeIf(u -> u.getId().equals(user.getId()));
+            data.add(user);
+            return user;
+        }
+    }
+
+    @Override
+    public Optional<User> findById(Long id) {
+        return data.stream()
+                .filter(u -> u.getId().equals(id))
+                .findAny();
+    }
+
+    @Override
+    public Optional<User> findByUsername(String username) {
+        return data.stream()
+                .filter(u -> u.getUsername().equals(username))
+                .findAny();
+    }
+}
