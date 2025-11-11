548648240fed7908ca096298331902ff649ef5c1
wooseop <useop0821@gmail.com>
2025-11-11 19:27:52 +0900

    test: 로그인 관련 테스트 추가

--- src/test/java/hello/delivery/user/service/UserServiceTest.java
+++ src/test/java/hello/delivery/user/service/UserServiceTest.java
@@ -1,7 +1,10 @@
 package hello.delivery.user.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import hello.delivery.common.exception.LoginException;
+import hello.delivery.common.exception.UserNotFound;
 import hello.delivery.mock.FakeUserRepository;
 import hello.delivery.user.domain.Login;
 import hello.delivery.user.domain.User;
@@ -17,7 +20,7 @@ class UserServiceTest {
 
     @BeforeEach
     void setUp() {
-         fakeUserRepository = new FakeUserRepository();
+        fakeUserRepository = new FakeUserRepository();
         userService = new UserService(fakeUserRepository);
     }
 
@@ -43,16 +46,16 @@ class UserServiceTest {
     }
 
     @Test
-    @DisplayName("")
+    @DisplayName("사용자는 username과 password로 로그인을 할 수 있다.")
     void login() throws Exception {
         // given
-        User user = User.builder()
+        UserCreate userCreate = UserCreate.builder()
                 .name("김우섭")
                 .username("wss3325")
                 .password("1234")
                 .address("대구")
                 .build();
-        fakeUserRepository.save(user);
+        userService.signup(userCreate);
         Login login = Login.builder()
                 .username("wss3325")
                 .password("1234")
@@ -65,5 +68,50 @@ class UserServiceTest {
         assertThat(loginUser.getUsername()).isEqualTo("wss3325");
         assertThat(loginUser.getPassword()).isEqualTo("1234");
     }
+
+    @Test
+    @DisplayName("아이디와 비밀번호 전부 틀리면 예외를 던진다.")
+    void invalidLogin() throws Exception {
+        // given
+        UserCreate userCreate = UserCreate.builder()
+                .name("김우섭")
+                .username("wss3325")
+                .password("1234")
+                .address("대구")
+                .build();
+        userService.signup(userCreate);
+        Login login = Login.builder()
+                .username("zzzz")
+                .password("11111")
+                .build();
+
+        // expect
+        assertThatThrownBy(() -> userService.login(login))
+                .isInstanceOf(UserNotFound.class)
+                .hasMessageContaining("사용자를 찾을 수 없습니다.");
+    }
+
+    @Test
+    @DisplayName("아이디나 비밀번호가 틀리면 예외를 던진다.")
+    void invalidLoginWrong() throws Exception {
+        // given
+        UserCreate userCreate = UserCreate.builder()
+                .name("김우섭")
+                .username("wss3325")
+                .password("1234")
+                .address("대구")
+                .build();
+        userService.signup(userCreate);
+        Login login = Login.builder()
+                .username("wss3325")
+                .password("11111")
+                .build();
+
+        // expect
+        assertThatThrownBy(() -> userService.login(login))
+                .isInstanceOf(LoginException.class)
+                .hasMessageContaining("아이디 또는 비밀번호가 일치하지 않습니다.");
+    }
+
 
 }
