55168e30ec4cf4fa6614ef3d5c16725144cbedbf
wooseop <useop0821@gmail.com>
2025-11-15 04:18:00 +0900

    feat: 비밀번호 변경 메서드 추가

--- src/main/java/hello/delivery/owner/domain/Owner.java
+++ src/main/java/hello/delivery/owner/domain/Owner.java
@@ -1,5 +1,6 @@
 package hello.delivery.owner.domain;
 
+import hello.delivery.owner.dto.OwnerCreate;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -21,6 +22,14 @@ public class Owner {
         return Owner.builder()
                 .name(ownerCreate.getName())
                 .password(ownerCreate.getPassword())
+                .build();
+    }
+
+    public Owner changePassword(String newPassword) {
+        return Owner.builder()
+                .id(id)
+                .name(name)
+                .password(newPassword)
                 .build();
     }
 }
