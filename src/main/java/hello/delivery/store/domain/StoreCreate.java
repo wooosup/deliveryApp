0f751205fcbf0b6a4b2371b911133401850230c8
wooseop <useop0821@gmail.com>
2025-11-16 21:46:36 +0900

    feat: 가게 생성 시 필수 입력 값에 대한 유효성 검사 추가

--- src/main/java/hello/delivery/store/domain/StoreCreate.java
+++ src/main/java/hello/delivery/store/domain/StoreCreate.java
@@ -2,6 +2,8 @@ package hello.delivery.store.domain;
 
 import hello.delivery.product.domain.ProductCreate;
 import hello.delivery.store.infrastructure.StoreType;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import java.util.List;
 import lombok.Builder;
 import lombok.Getter;
@@ -9,9 +11,15 @@ import lombok.Getter;
 @Getter
 public class StoreCreate {
 
+    @NotNull(message = "가게 주인은 필수 입력 값입니다.")
     private final long ownerId;
+
+    @NotBlank(message = "가게이름은 필수 입력 값입니다.")
     private final String storeName;
+
+    @NotNull(message = "가게타입은 필수 입력 값입니다.")
     private final StoreType storeType;
+
     private final List<ProductCreate> products;
 
     @Builder
