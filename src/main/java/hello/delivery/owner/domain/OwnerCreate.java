532ad76a8b8657e6e26f1bca3366bc3b4dda0a9a
wooseop <useop0821@gmail.com>
2025-11-16 21:51:13 +0900

    chore: 비밀번호 및 이름 필드에 대한 유효성 검사 로직 개선

--- src/main/java/hello/delivery/owner/domain/OwnerCreate.java
+++ src/main/java/hello/delivery/owner/domain/OwnerCreate.java
@@ -1,18 +1,19 @@
 package hello.delivery.owner.domain;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Builder;
 import lombok.Getter;
-import org.hibernate.validator.constraints.Length;
 
 @Getter
 public class OwnerCreate {
 
     @NotBlank(message = "이름은 필수 입력 값입니다.")
+    @Size(max = 4, message = "이름은 최대 4자까지 입력 가능합니다.")
     private final String name;
 
     @NotBlank(message = "비밀번호는 필수 입력 값입니다.")
-    @Length(min = 8, max = 20, message = "비밀번호는 8자 이상 20자 이하로 입력 가능합니다.")
+    @Size(min = 8, max = 20, message = "비밀번호는 8자 이상 20자 이하로 입력 가능합니다.")
     private final String password;
 
     @Builder
--- src/main/java/hello/delivery/user/domain/UserCreate.java
+++ src/main/java/hello/delivery/user/domain/UserCreate.java
@@ -1,23 +1,23 @@
 package hello.delivery.user.domain;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Builder;
 import lombok.Getter;
-import org.hibernate.validator.constraints.Length;
 
 @Getter
 public class UserCreate {
 
     @NotBlank(message = "이름은 필수 입력 값입니다.")
-    @Length(max = 4, message = "이름은 최대 4자까지 입력 가능합니다.")
+    @Size(max = 4, message = "이름은 최대 4자까지 입력 가능합니다.")
     private final String name;
 
     @NotBlank(message = "아이디는 필수 입력 값입니다.")
-    @Length(min = 5, max = 20, message = "아이디는 5자 이상 20자 이하로 입력 가능합니다.")
+    @Size(min = 5, max = 20, message = "아이디는 5자 이상 20자 이하로 입력 가능합니다.")
     private final String username;
 
     @NotBlank(message = "비밀번호는 필수 입력 값입니다.")
-    @Length(min = 8, max = 20, message = "비밀번호는 8자 이상 20자 이하로 입력 가능합니다.")
+    @Size(min = 8, max = 20, message = "비밀번호는 8자 이상 20자 이하로 입력 가능합니다.")
     private final String password;
 
     @NotBlank(message = "주소는 필수 입력 값입니다.")
