5ac85c03a75bb3f88b5c1589273351351a5ebf43
wooseop <useop0821@gmail.com>
2025-11-17 15:03:21 +0900

    refactor: 상품을 추가하는 것은 StoreService의 책임이 아니라 생각하여 삭제

--- src/main/java/hello/delivery/store/service/StoreService.java
+++ src/main/java/hello/delivery/store/service/StoreService.java
@@ -3,9 +3,6 @@ package hello.delivery.store.service;
 import hello.delivery.common.service.port.ClockHolder;
 import hello.delivery.common.service.port.FinderPort;
 import hello.delivery.owner.domain.Owner;
-import hello.delivery.product.domain.Product;
-import hello.delivery.product.domain.ProductCreate;
-import hello.delivery.product.service.ProductService;
 import hello.delivery.store.domain.Store;
 import hello.delivery.store.domain.StoreCreate;
 import hello.delivery.store.infrastructure.StoreType;
@@ -21,29 +18,19 @@ import org.springframework.transaction.annotation.Transactional;
 public class StoreService {
 
     private final StoreRepository storeRepository;
-    private final ProductService productService;
     private final FinderPort finder;
     private final ClockHolder clockHolder;
 
     @Transactional
     public Store create(Long ownerId, StoreCreate request) {
         Owner owner = finder.findByOwner(ownerId);
-
         Store store = Store.of(request, owner, clockHolder.now());
+
         return storeRepository.save(store);
     }
 
     public List<Store> findByStoreType(StoreType storeType) {
         return storeRepository.findByStoreType(storeType);
-    }
-
-    @Transactional
-    public Product addProduct(Long storeId, ProductCreate request) {
-        Store store = finder.findByStore(storeId);
-        Product product = productService.create(store, request);
-
-        store.addProduct(product);
-        return product;
     }
 
 }
