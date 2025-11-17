4fab8fded7db9915077cd9efd8d2affbdc85b2bb
wooseop <useop0821@gmail.com>
2025-11-17 17:47:01 +0900

    feat: OrderController 클래스 추가하여 주문 생성 API 기능 구현

---
+++ src/main/java/hello/delivery/order/controller/OrderController.java
@@ -0,0 +1,27 @@
+package hello.delivery.order.controller;
+
+
+import hello.delivery.common.api.ApiResponse;
+import hello.delivery.order.controller.response.OrderResponse;
+import hello.delivery.order.domain.OrderCreate;
+import hello.delivery.order.service.OrderService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/orders")
+@RequiredArgsConstructor
+public class OrderController {
+
+    private final OrderService orderService;
+
+    @PostMapping("/new")
+    public ApiResponse<OrderResponse> order(@Valid @RequestParam OrderCreate request) {
+        return ApiResponse.ok(OrderResponse.of(orderService.order(request)));
+    }
+
+}
