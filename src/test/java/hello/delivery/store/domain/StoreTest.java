d94bbf40bbb97b5191c674493639076e6fa3e4f4
wooseop <useop0821@gmail.com>
2025-11-17 15:40:11 +0900

    refactor: addProduct테스트 제거

--- src/test/java/hello/delivery/store/domain/StoreTest.java
+++ src/test/java/hello/delivery/store/domain/StoreTest.java
@@ -1,14 +1,10 @@
 package hello.delivery.store.domain;
 
-import static hello.delivery.product.infrastructure.ProductType.FOOD;
 import static hello.delivery.store.infrastructure.StoreType.KOREAN_FOOD;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-import hello.delivery.common.exception.DuplicateProductException;
 import hello.delivery.mock.TestClockHolder;
 import hello.delivery.owner.domain.Owner;
-import hello.delivery.product.domain.Product;
 import java.time.LocalDate;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -81,63 +77,6 @@ class StoreTest {
         assertThat(secondSale.getTotalSales()).isEqualTo(20000);
         assertThat(secondSale.getDailySales()).isEqualTo(10000);
         assertThat(secondSale.getLastSalesDate()).isEqualTo(today);
-    }
-
-    @Test
-    @DisplayName("Store에 Product를 추가한다.")
-    void addProduct() throws Exception {
-        // given
-        LocalDate now = new TestClockHolder().now();
-        Owner owner = buildOwner();
-        StoreCreate storeCreate = StoreCreate.builder()
-                .ownerId(owner.getId())
-                .storeType(KOREAN_FOOD)
-                .storeName("한식당")
-                .build();
-        Store store = Store.of(storeCreate, owner, now);
-        Product product = Product.builder()
-                .name("치킨")
-                .store(store)
-                .productType(FOOD)
-                .price(20000)
-                .build();
-
-        // when
-        store.addProduct(product);
-
-        // then
-        assertThat(store.getProducts()).hasSize(1);
-        assertThat(store.getProducts().get(0).getName()).isEqualTo("치킨");
-        assertThat(store.getProducts().get(0).getPrice()).isEqualTo(20000);
-        assertThat(store.getProducts().get(0).getStore()).isEqualTo(store);
-        assertThat(store.getProducts().get(0).getProductType()).isEqualTo(FOOD);
-    }
-
-    @Test
-    @DisplayName("중복된 Product를 추가하면 예외가 발생한다.")
-    void duplicateAddProduct() throws Exception {
-        // given
-        LocalDate now = new TestClockHolder().now();
-        Owner owner = buildOwner();
-        StoreCreate storeCreate = StoreCreate.builder()
-                .ownerId(owner.getId())
-                .storeType(KOREAN_FOOD)
-                .storeName("한식당")
-                .build();
-        Store store = Store.of(storeCreate, owner, now);
-        Product product = Product.builder()
-                .name("치킨")
-                .store(store)
-                .productType(FOOD)
-                .price(20000)
-                .build();
-        store.addProduct(product);
-
-        // expect
-        assertThatThrownBy(() -> store.addProduct(product))
-                .isInstanceOf(DuplicateProductException.class)
-                .hasMessageContaining("이미 존재하는 상품입니다.");
-
     }
 
     private static Owner buildOwner() {
