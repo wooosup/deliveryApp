6bf393279c77e97f0e7346b1b43c1bca1fba0bda
wooseop <useop0821@gmail.com>
2025-11-18 18:43:28 +0900

    feat: Swagger 설정 추가

---
+++ src/main/java/hello/delivery/common/config/SwaggerConfig.java
@@ -0,0 +1,20 @@
+package hello.delivery.common.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        Info info = new Info()
+                .version("v1.0")
+                .title("Delivery API")
+                .description("배달 API");
+        return new OpenAPI()
+                .info(info);
+    }
+}
