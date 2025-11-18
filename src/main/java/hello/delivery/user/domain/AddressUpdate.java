3e6cdcd58a6663dc352f2fbe31bbfdaca8c71287
wooseop <useop0821@gmail.com>
2025-11-18 17:25:23 +0900

    feat: 주소 및 비밀번호 업데이트 클래스에 유효성 검사 추가

--- src/main/java/hello/delivery/user/domain/AddressUpdate.java
+++ src/main/java/hello/delivery/user/domain/AddressUpdate.java
@@ -1,12 +1,14 @@
 package hello.delivery.user.domain;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 public class AddressUpdate {
 
+    @NotBlank(message = "주소는 필수 입력 값입니다.")
     private final String address;
 
     @Builder
--- src/main/java/hello/delivery/user/domain/PasswordUpdate.java
+++ src/main/java/hello/delivery/user/domain/PasswordUpdate.java
@@ -1,12 +1,14 @@
 package hello.delivery.user.domain;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.Size;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 public class PasswordUpdate {
 
+    @Size(min = 8, max = 20, message = "비밀번호는 8자 이상 20자 이하로 입력 가능합니다.")
     private final String password;
 
     @Builder
