eac43003b149a842c4ae5145dd26b43e00828d4c
wooseop <useop0821@gmail.com>
2025-11-18 17:38:32 +0900

    feat: 로깅 추가

--- src/main/java/hello/delivery/common/controller/ExceptionController.java
+++ src/main/java/hello/delivery/common/controller/ExceptionController.java
@@ -5,6 +5,7 @@ import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
 
 import hello.delivery.common.exception.DeliveryException;
 import hello.delivery.common.exception.ErrorResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -12,12 +13,14 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@Slf4j
 @RestControllerAdvice
 public class ExceptionController {
 
     @ResponseStatus(BAD_REQUEST)
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ErrorResponse invalidRequestHandler(MethodArgumentNotValidException e) {
+        log.error("MethodArgumentNotValidException 발생: {}", e.getMessage(), e);
         ErrorResponse response = ErrorResponse.builder()
                 .code("400")
                 .message("잘못된 요청입니다.")
@@ -32,6 +35,7 @@ public class ExceptionController {
 
     @ExceptionHandler(DeliveryException.class)
     public ResponseEntity<ErrorResponse> deliveryException(DeliveryException e) {
+        log.error("DeliveryException 발생: {}", e.getMessage(), e);
         ErrorResponse response = ErrorResponse.builder()
                 .code(String.valueOf(e.getStatus().value()))
                 .message(e.getMessage())
@@ -42,7 +46,8 @@ public class ExceptionController {
 
     @ResponseStatus(INTERNAL_SERVER_ERROR)
     @ExceptionHandler(Exception.class)
-    public ErrorResponse generalServerError() {
+    public ErrorResponse generalServerError(Exception e) {
+        log.error("알 수 없는 서버 에러 발생", e);
         return ErrorResponse.builder()
                 .code("500")
                 .message("서버에 오류가 발생했습니다.")
