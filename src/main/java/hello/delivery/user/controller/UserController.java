da7fa66f13108596c05a2b780302bca2a272218a
wooseop <useop0821@gmail.com>
2025-11-18 17:39:52 +0900

    feat: 사용자 회원가입, 로그인, 주소 및 비밀번호 변경 기능 추가

--- src/main/java/hello/delivery/user/controller/UserController.java
+++ src/main/java/hello/delivery/user/controller/UserController.java
@@ -6,16 +6,25 @@ import hello.delivery.user.controller.response.UserResponse;
 import hello.delivery.user.domain.AddressUpdate;
 import hello.delivery.user.domain.Login;
 import hello.delivery.user.domain.PasswordUpdate;
+import hello.delivery.user.domain.User;
 import hello.delivery.user.domain.UserCreate;
 import hello.delivery.user.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "사용자 (user, owner)")
 @RestController
 @RequestMapping("/api/users")
 @RequiredArgsConstructor
@@ -24,23 +33,45 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping("/signup")
-    public ApiResponse<UserResponse> signup(@Valid @RequestBody UserCreate request) {
-        return ApiResponse.ok(UserResponse.of(userService.signupCustomer(request)));
+    @Operation(summary = "고객 회원가입", description = "고객으로 회원가입을 진행합니다.")
+    public ApiResponse<UserResponse> signupCustomer(@Valid @RequestBody UserCreate request) {
+        User user = userService.signupCustomer(request);
+        return ApiResponse.ok(UserResponse.of(user));
+    }
+
+    @PostMapping("/owners/signup")
+    @Operation(summary = "사장님 회원가입", description = "사장님으로 회원가입을 진행합니다.")
+    public ApiResponse<UserResponse> signupOwner(@Valid @RequestBody UserCreate request) {
+        User user = userService.signupOwner(request);
+        return ApiResponse.ok(UserResponse.of(user));
     }
 
     @PostMapping("/login")
-    public ApiResponse<UserResponse> login(@Valid @RequestBody Login request) {
-        return ApiResponse.ok(UserResponse.of(userService.login(request)));
+    @Operation(summary = "로그인", description = "아이디와 비밀번호로 로그인을 진행합니다.")
+    public ApiResponse<UserResponse> login(@Valid @RequestBody Login request, HttpServletRequest httpServletRequest) {
+        User user = userService.login(request);
+        HttpSession session = httpServletRequest.getSession(true);
+        session.setAttribute("userId", user.getId());
+
+        return ApiResponse.ok(UserResponse.of(user));
     }
 
-    @PostMapping("/change-address/{id}")
-    public ApiResponse<UserResponse> changeAddress(@PathVariable Long id, @RequestBody AddressUpdate request) {
-        return ApiResponse.ok(UserResponse.of(userService.changeAddress(id, request)));
+    @PatchMapping("/address")
+    @Parameter(in = ParameterIn.HEADER, name = "USERNAME")
+    @Operation(summary = "주소 변경", description = "사용자의 주소를 변경합니다.")
+    public ApiResponse<UserResponse> changeAddress(@Parameter(name = "USERNAME", in = ParameterIn.HEADER) @RequestHeader("USERNAME") String username, @Valid @RequestBody AddressUpdate request) {
+        User user = userService.getByUsername(username);
+        user = userService.changeAddress(user.getId(), request);
+        return ApiResponse.ok(UserResponse.of(user));
     }
 
-    @PostMapping("/change-password/{id}")
-    public ApiResponse<UserResponse> changePassword(@PathVariable Long id, @RequestBody PasswordUpdate request) {
-        return ApiResponse.ok(UserResponse.of(userService.changePassword(id, request)));
+    @PatchMapping("/password")
+    @Parameter(in = ParameterIn.HEADER, name = "USERNAME")
+    @Operation(summary = "비밀번호 변경", description = "사용자의 비밀번호를 변경합니다.")
+    public ApiResponse<UserResponse> changePassword(@Parameter(name = "USERNAME", in = ParameterIn.HEADER) @RequestHeader("USERNAME") String username, @Valid @RequestBody PasswordUpdate request) {
+        User user = userService.getByUsername(username);
+        user = userService.changePassword(user.getId(), request);
+        return ApiResponse.ok(UserResponse.of(user));
     }
 
 }
