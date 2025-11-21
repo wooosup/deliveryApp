1d2566c96717a7f0ea731d72f5ce55aa1ac6fb85
wooseop <useop0821@gmail.com>
2025-11-21 22:06:07 +0900

    feat: 주문 응답에 가게 ID 및 주문 시간을 추가하고 가게 이름으로 변경

--- src/main/java/hello/delivery/order/controller/response/OrderResponse.java
+++ src/main/java/hello/delivery/order/controller/response/OrderResponse.java
@@ -1,7 +1,7 @@
 package hello.delivery.order.controller.response;
 
 import hello.delivery.order.domain.Order;
-import hello.delivery.store.controller.response.StoreResponse;
+import java.time.LocalDateTime;
 import java.util.List;
 import lombok.Builder;
 import lombok.Getter;
@@ -10,26 +10,33 @@ import lombok.Getter;
 public class OrderResponse {
 
     private final Long id;
+    private final Long storeId;
     private final int totalPrice;
     private final String address;
-    private final StoreResponse store;
+    private final String storeName;
+    private final LocalDateTime orderedAt;
     private final List<OrderProductResponse> orderProducts;
 
     @Builder
-    private OrderResponse(Long id, int totalPrice, String address, StoreResponse store, List<OrderProductResponse> orderProducts) {
+    private OrderResponse(Long id, Long storeId, int totalPrice, String address, String storeName,
+                          LocalDateTime orderedAt, List<OrderProductResponse> orderProducts) {
         this.id = id;
+        this.storeId = storeId;
         this.totalPrice = totalPrice;
         this.address = address;
-        this.store = store;
+        this.storeName = storeName;
+        this.orderedAt = orderedAt;
         this.orderProducts = orderProducts;
     }
 
     public static OrderResponse of(Order order) {
         return OrderResponse.builder()
                 .id(order.getId())
+                .storeId(order.getStore().getId())
                 .totalPrice(order.getTotalPrice())
                 .address(order.getUser().getAddress())
-                .store(StoreResponse.of(order.getStore()))
+                .storeName(order.getStore().getName())
+                .orderedAt(order.getOrderedAt())
                 .orderProducts(order.getOrderProducts().stream()
                         .map(OrderProductResponse::of)
                         .toList())
