b6b48979f6f75fa75f2e87f8d96a884b9555e0dc
wooseop <useop0821@gmail.com>
2025-11-21 21:53:10 +0900

    feat: 가게 생성 및 총 판매 추가 기능 구현, 중복 가게 이름 검증 추가

--- src/main/java/hello/delivery/store/service/StoreService.java
+++ src/main/java/hello/delivery/store/service/StoreService.java
@@ -1,5 +1,6 @@
 package hello.delivery.store.service;
 
+import hello.delivery.common.exception.StoreException;
 import hello.delivery.common.exception.StoreNotFound;
 import hello.delivery.common.service.port.ClockHolder;
 import hello.delivery.common.service.port.FinderPort;
@@ -23,11 +24,21 @@ public class StoreService {
     private final ClockHolder clockHolder;
 
     @Transactional
-    public Store create(StoreCreate request) {
-        User owner = finder.findByOwner(request.getOwnerId());
-        Store store = Store.of(request, owner, clockHolder.now());
+    public Store create(Long userId, StoreCreate request) {
+        User owner = finder.findByUser(userId);
+        validateDuplicate(request);
+
+        Store store = Store.create(request, owner, clockHolder.now());
 
         return storeRepository.save(store);
+    }
+
+    @Transactional
+    public void addTotalSales(Long storeId, int amount) {
+        Store store = finder.findByStore(storeId);
+        Store updatedStore = store.addTotalSales(amount, clockHolder.now());
+
+        repositoryUpdate(store, updatedStore);
     }
 
     public List<Store> findByStoreType(StoreType storeType) {
@@ -41,6 +52,28 @@ public class StoreService {
     public Store findByName(String name) {
         return storeRepository.findByName(name)
                 .orElseThrow(StoreNotFound::new);
+    }
+
+    public List<Store> findByOwnerId(Long userId) {
+        User owner = finder.findByUser(userId);
+        owner.validateOwnerRole();
+
+        return storeRepository.findByOwner(owner);
+    }
+
+    private void validateDuplicate(StoreCreate request) {
+        if (storeRepository.existsByName(request.getStoreName())) {
+            throw new StoreException("이미 존재하는 가게 이름입니다.");
+        }
+    }
+
+    private void repositoryUpdate(Store store, Store updatedStore) {
+        storeRepository.updateSales(
+                store.getId(),
+                updatedStore.getDailySales(),
+                updatedStore.getTotalSales(),
+                updatedStore.getLastSalesDate()
+        );
     }
 
 }
