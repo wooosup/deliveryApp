eee29edc16aea3952a0227831d73fb7092d6aee2
wooseop <useop0821@gmail.com>
2025-11-21 22:05:37 +0900

    refactor: 생성을 쉽게 하기 위해서 id에서 name으로 변경

--- src/main/java/hello/delivery/order/domain/OrderProductRequest.java
+++ src/main/java/hello/delivery/order/domain/OrderProductRequest.java
@@ -1,20 +1,27 @@
 package hello.delivery.order.domain;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 public class OrderProductRequest {
 
-    private final long productId;
+    @NotBlank(message = "상품 이름은 필수 입력 값입니다.")
+    private final String productName;
+
+    @NotNull(message = "상품 개수는 필수 입력 값입니다.")
+    @Positive(message = "상품 개수는 양수여야 합니다.")
     private final int quantity;
 
     @Builder
     private OrderProductRequest(
-            @JsonProperty("productId") long productId,
+            @JsonProperty("productName") String productName,
             @JsonProperty("quantity") int quantity) {
-        this.productId = productId;
+        this.productName = productName;
         this.quantity = quantity;
     }
 }
