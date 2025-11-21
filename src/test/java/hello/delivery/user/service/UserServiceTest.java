653213bfed326b3c78b7d5dce14350ae2b77ce27
wooseop <useop0821@gmail.com>
2025-11-21 21:48:58 +0900

    refactor: UserServiceTest에서 중복된 사용자 생성 및 로그인 요청 코드를 메서드로 추출

--- src/test/java/hello/delivery/user/service/UserServiceTest.java
+++ src/test/java/hello/delivery/user/service/UserServiceTest.java
@@ -21,6 +21,14 @@ class UserServiceTest {
 
     private UserService userService;
 
+    private static final String DEFAULT_NAME = "김우섭";
+    private static final String DEFAULT_USERNAME = "wss3325";
+    private static final String DEFAULT_PASSWORD = "hihihi3454";
+    private static final String DEFAULT_ADDRESS = "대구";
+    private static final String NEW_ADDRESS = "서울";
+    private static final String NEW_PASSWORD = "hihihi9999";
+    private static final String INVALID_SHORT_PASSWORD = "9999";
+
     @BeforeEach
     void setUp() {
         FakeUserRepository fakeUserRepository = new FakeUserRepository();
@@ -29,179 +37,119 @@ class UserServiceTest {
 
     @Test
     @DisplayName("고객으로 회원가입을 할 수 있다.")
-    void signupCustomer() throws Exception {
+    void signupCustomer() {
         // given
-        UserCreate userCreate = UserCreate.builder()
-                .name("김우섭")
-                .username("wss3325")
-                .password("hihihi3454")
-                .address("대구")
-                .build();
+        UserCreate userCreate = createUserCreate();
 
         // when
         User result = userService.signupCustomer(userCreate);
 
         // then
-        assertThat(result.getName()).isEqualTo("김우섭");
-        assertThat(result.getUsername()).isEqualTo("wss3325");
-        assertThat(result.getAddress()).isEqualTo("대구");
+        assertThat(result.getName()).isEqualTo(DEFAULT_NAME);
+        assertThat(result.getUsername()).isEqualTo(DEFAULT_USERNAME);
+        assertThat(result.getAddress()).isEqualTo(DEFAULT_ADDRESS);
         assertThat(result.getRole()).isEqualTo(CUSTOMER);
     }
 
     @Test
     @DisplayName("사장으로 회원가입을 할 수 있다.")
-    void signupOwner() throws Exception {
+    void signupOwner() {
         // given
-        UserCreate userCreate = UserCreate.builder()
-                .name("김우섭")
-                .username("wss3325")
-                .password("hihihi3454")
-                .address("대구")
-                .build();
+        UserCreate userCreate = createUserCreate();
 
         // when
         User result = userService.signupOwner(userCreate);
 
         // then
-        assertThat(result.getName()).isEqualTo("김우섭");
-        assertThat(result.getUsername()).isEqualTo("wss3325");
-        assertThat(result.getAddress()).isEqualTo("대구");
+        assertThat(result.getName()).isEqualTo(DEFAULT_NAME);
+        assertThat(result.getUsername()).isEqualTo(DEFAULT_USERNAME);
+        assertThat(result.getAddress()).isEqualTo(DEFAULT_ADDRESS);
         assertThat(result.getRole()).isEqualTo(OWNER);
     }
 
     @Test
     @DisplayName("사용자는 아이디와 비밀번호로 로그인을 할 수 있다.")
-    void login() throws Exception {
+    void login() {
         // given
-        UserCreate userCreate = UserCreate.builder()
-                .name("김우섭")
-                .username("wss3325")
-                .password("hihihi3454")
-                .address("대구")
-                .build();
+        UserCreate userCreate = createUserCreate();
         userService.signupCustomer(userCreate);
-        Login user = Login.builder()
-                .username("wss3325")
-                .password("hihihi3454")
-                .build();
+        Login loginRequest = createLoginRequest(DEFAULT_USERNAME, DEFAULT_PASSWORD);
 
         // when
-        User result = userService.login(user);
+        User result = userService.login(loginRequest);
 
         // then
-        assertThat(result.getUsername()).isEqualTo("wss3325");
+        assertThat(result.getUsername()).isEqualTo(DEFAULT_USERNAME);
     }
 
     @Test
     @DisplayName("아이디와 비밀번호 전부 틀리면 예외를 던진다.")
-    void invalidLogin() throws Exception {
+    void invalidLogin() {
         // given
-        UserCreate userCreate = UserCreate.builder()
-                .name("김우섭")
-                .username("wss3325")
-                .password("hihihi3454")
-                .address("대구")
-                .build();
+        UserCreate userCreate = createUserCreate();
         userService.signupCustomer(userCreate);
-        Login user = Login.builder()
-                .username("zzzzzzz")
-                .password("hihihi1111")
-                .build();
+        Login loginRequest = createLoginRequest("zzzzzzz", "hihihi1111");
 
         // expect
-        assertThatThrownBy(() -> userService.login(user))
+        assertThatThrownBy(() -> userService.login(loginRequest))
                 .isInstanceOf(UserNotFound.class)
                 .hasMessageContaining("사용자를 찾을 수 없습니다.");
     }
 
     @Test
     @DisplayName("아이디나 비밀번호가 틀리면 예외를 던진다.")
-    void invalidLoginWrong() throws Exception {
+    void invalidLoginWrong() {
         // given
-        UserCreate userCreate = UserCreate.builder()
-                .name("김우섭")
-                .username("wss3325")
-                .password("hihihi3454")
-                .address("대구")
-                .build();
+        UserCreate userCreate = createUserCreate();
         userService.signupCustomer(userCreate);
-        Login user = Login.builder()
-                .username("wss3325")
-                .password("hihihi1111")
-                .build();
+        Login loginRequest = createLoginRequest(DEFAULT_USERNAME, "hihihi1111");
 
         // expect
-        assertThatThrownBy(() -> userService.login(user))
+        assertThatThrownBy(() -> userService.login(loginRequest))
                 .isInstanceOf(UserException.class)
                 .hasMessageContaining("아이디 또는 비밀번호가 일치하지 않습니다.");
     }
 
-
     @Test
     @DisplayName("사용자는 주소를 변경할 수 있다.")
-    void changeAddress() throws Exception {
+    void changeAddress() {
         // given
-        UserCreate userCreate = UserCreate.builder()
-                .name("김우섭")
-                .username("wss3325")
-                .password("hihihi3454")
-                .address("대구")
-                .build();
+        UserCreate userCreate = createUserCreate();
         User user = userService.signupCustomer(userCreate);
-        AddressUpdate addressUpdate = AddressUpdate.builder()
-                .address("서울")
-                .build();
+        AddressUpdate addressUpdate = createAddressUpdate(NEW_ADDRESS);
 
         // when
         User result = userService.changeAddress(user.getId(), addressUpdate);
 
         // then
-        assertThat(result.getUsername()).isEqualTo("wss3325");
-        assertThat(result.getAddress()).isEqualTo("서울");
+        assertThat(result.getUsername()).isEqualTo(DEFAULT_USERNAME);
+        assertThat(result.getAddress()).isEqualTo(NEW_ADDRESS);
     }
-
 
     @Test
     @DisplayName("사용자는 비밀번호를 변경할 수 있다.")
-    void changePassword() throws Exception {
+    void changePassword() {
         // given
-        UserCreate userCreate = UserCreate.builder()
-                .name("김우섭")
-                .username("wss3325")
-                .password("hihihi3454")
-                .address("대구")
-                .build();
+        UserCreate userCreate = createUserCreate();
         User user = userService.signupCustomer(userCreate);
-        PasswordUpdate passwordUpdate = PasswordUpdate.builder()
-                .password("hihihi9999")
-                .build();
+        PasswordUpdate passwordUpdate = createPasswordUpdate(NEW_PASSWORD);
 
         // when
         userService.changePassword(user.getId(), passwordUpdate);
-        Login result = Login.builder()
-                .username("wss3325")
-                .password("hihihi9999")
-                .build();
+        Login loginRequest = createLoginRequest(DEFAULT_USERNAME, NEW_PASSWORD);
 
         // then
-        assertThat(result.getUsername()).isEqualTo("wss3325");
-        assertThat(result.getPassword()).isEqualTo("hihihi9999");
+        assertThat(loginRequest.getUsername()).isEqualTo(DEFAULT_USERNAME);
+        assertThat(loginRequest.getPassword()).isEqualTo(NEW_PASSWORD);
     }
 
     @Test
-    @DisplayName("비밀변호는 8자 이상 20자 이하로 입력해야 한다.")
-    void validateLengthChangePassword() throws Exception {
+    @DisplayName("비밀번호는 8자 이상 20자 이하로 입력해야 한다.")
+    void validateLengthChangePassword() {
         // given
-        UserCreate userCreate = UserCreate.builder()
-                .name("김우섭")
-                .username("wss3325")
-                .password("hihihi3454")
-                .address("대구")
-                .build();
+        UserCreate userCreate = createUserCreate();
         User user = userService.signupCustomer(userCreate);
-        PasswordUpdate passwordUpdate = PasswordUpdate.builder()
-                .password("9999")
-                .build();
+        PasswordUpdate passwordUpdate = createPasswordUpdate(INVALID_SHORT_PASSWORD);
 
         // expect
         assertThatThrownBy(() -> userService.changePassword(user.getId(), passwordUpdate))
@@ -210,19 +158,12 @@ class UserServiceTest {
     }
 
     @Test
-    @DisplayName("비밀변호 변경 시 기존 비밀번호와 같으면 예외가 발생한다.")
-    void validateSamePassword() throws Exception {
+    @DisplayName("비밀번호 변경 시 기존 비밀번호와 같으면 예외가 발생한다.")
+    void validateSamePassword() {
         // given
-        UserCreate userCreate = UserCreate.builder()
-                .name("김우섭")
-                .username("wss3325")
-                .password("hihihi3454")
-                .address("대구")
-                .build();
+        UserCreate userCreate = createUserCreate();
         User user = userService.signupCustomer(userCreate);
-        PasswordUpdate passwordUpdate = PasswordUpdate.builder()
-                .password("hihihi3454")
-                .build();
+        PasswordUpdate passwordUpdate = createPasswordUpdate(DEFAULT_PASSWORD);
 
         // expect
         assertThatThrownBy(() -> userService.changePassword(user.getId(), passwordUpdate))
@@ -230,4 +171,31 @@ class UserServiceTest {
                 .hasMessageContaining("기존 비밀번호와 동일한 비밀번호로 변경할 수 없습니다.");
     }
 
+    private UserCreate createUserCreate() {
+        return UserCreate.builder()
+                .name(DEFAULT_NAME)
+                .username(DEFAULT_USERNAME)
+                .password(DEFAULT_PASSWORD)
+                .address(DEFAULT_ADDRESS)
+                .build();
+    }
+
+    private Login createLoginRequest(String username, String password) {
+        return Login.builder()
+                .username(username)
+                .password(password)
+                .build();
+    }
+
+    private AddressUpdate createAddressUpdate(String address) {
+        return AddressUpdate.builder()
+                .address(address)
+                .build();
+    }
+
+    private PasswordUpdate createPasswordUpdate(String password) {
+        return PasswordUpdate.builder()
+                .password(password)
+                .build();
+    }
 }
