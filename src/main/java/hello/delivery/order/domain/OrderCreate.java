e8108ffafa67072ccef9a9746e2cc1436103aec9
wooseop <useop0821@gmail.com>
2025-11-21 22:01:18 +0900

    feat: 주문 생성 시 가게 이름 및 주문 상품 필수 검증 추가

--- src/main/java/hello/delivery/order/domain/OrderCreate.java
+++ src/main/java/hello/delivery/order/domain/OrderCreate.java
@@ -1,6 +1,8 @@
 package hello.delivery.order.domain;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import java.util.List;
 import lombok.Builder;
 import lombok.Getter;
@@ -8,17 +10,17 @@ import lombok.Getter;
 @Getter
 public class OrderCreate {
 
-    private final long storeId;
-    private final long userId;
+    @NotBlank(message = "가게 이름은 필수 입력 값입니다.")
+    private final String storeName;
+
+    @NotEmpty(message = "주문 상품은 필수 입력 값입니다.")
     private final List<OrderProductRequest> orderProducts;
 
     @Builder
     private OrderCreate(
-            @JsonProperty("storeId") long storeId,
-            @JsonProperty("userId") long userId,
+            @JsonProperty("storeName") String storeName,
             @JsonProperty("orderProducts") List<OrderProductRequest> orderProducts) {
-        this.storeId = storeId;
-        this.userId = userId;
+        this.storeName = storeName;
         this.orderProducts = orderProducts;
     }
 
