7d7bc9641e006b8b154a459e7a6fc706d6d0622b
wooseop <useop0821@gmail.com>
2025-11-23 04:35:13 +0900

    feat: findById 추가

--- src/main/java/hello/delivery/order/infrastructure/OrderRepositoryImpl.java
+++ src/main/java/hello/delivery/order/infrastructure/OrderRepositoryImpl.java
@@ -3,6 +3,7 @@ package hello.delivery.order.infrastructure;
 import hello.delivery.order.domain.Order;
 import hello.delivery.order.service.port.OrderRepository;
 import java.util.List;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
@@ -15,6 +16,11 @@ public class OrderRepositoryImpl implements OrderRepository {
     @Override
     public Order save(Order order) {
         return orderJpaRepository.save(OrderEntity.of(order)).toDomain();
+    }
+
+    @Override
+    public Optional<Order> findById(Long id) {
+        return orderJpaRepository.findById(id).map(OrderEntity::toDomain);
     }
 
     @Override
--- src/main/java/hello/delivery/order/service/port/OrderRepository.java
+++ src/main/java/hello/delivery/order/service/port/OrderRepository.java
@@ -2,10 +2,13 @@ package hello.delivery.order.service.port;
 
 import hello.delivery.order.domain.Order;
 import java.util.List;
+import java.util.Optional;
 
 public interface OrderRepository {
 
     Order save(Order order);
+
+    Optional<Order> findById(Long id);
 
     List<Order> findOrdersByUserId(long userId);
 
