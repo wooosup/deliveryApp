f49e1fd8532265a20b40e581fdeb64157148be10
wooseop <useop0821@gmail.com>
2025-11-23 04:35:35 +0900

    test: delivery 관련 추가 구현

--- src/test/java/hello/delivery/mock/FakeFinder.java
+++ src/test/java/hello/delivery/mock/FakeFinder.java
@@ -1,9 +1,11 @@
 package hello.delivery.mock;
 
+import hello.delivery.common.exception.DeliveryNotFound;
 import hello.delivery.common.exception.ProductNotFound;
 import hello.delivery.common.exception.StoreNotFound;
 import hello.delivery.common.exception.UserNotFound;
 import hello.delivery.common.service.port.FinderPort;
+import hello.delivery.delivery.domain.Delivery;
 import hello.delivery.product.domain.Product;
 import hello.delivery.store.domain.Store;
 import hello.delivery.user.domain.User;
@@ -15,6 +17,7 @@ public class FakeFinder implements FinderPort {
     private final List<User> users = new ArrayList<>();
     private final List<Store> stores = new ArrayList<>();
     private final List<Product> products = new ArrayList<>();
+    private final List<Delivery> deliveries = new ArrayList<>();
 
     public void addUser(User user) {
         users.add(user);
@@ -28,11 +31,23 @@ public class FakeFinder implements FinderPort {
         products.add(product);
     }
 
+    public void addDelivery(Delivery delivery) {
+        deliveries.add(delivery);
+    }
+
     public User findByUser(Long userId) {
         return users.stream()
                 .filter(user -> user.getId().equals(userId))
                 .findFirst()
                 .orElseThrow(UserNotFound::new);
+    }
+
+    @Override
+    public Delivery findByDelivery(Long id) {
+        return deliveries.stream()
+                .filter(delivery -> delivery.getId().equals(id))
+                .findFirst()
+                .orElseThrow(DeliveryNotFound::new);
     }
 
     public Store findByStoreName(String storeName) {
