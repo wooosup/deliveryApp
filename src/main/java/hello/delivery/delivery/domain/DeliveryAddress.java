e45b3dc2fd4b43ca81ab53c4ffe3a213986437bc
wooseop <useop0821@gmail.com>
2025-11-23 04:22:41 +0900

    feat: 검증 및 of 메서드 구현

--- src/main/java/hello/delivery/delivery/domain/DeliveryAddress.java
+++ src/main/java/hello/delivery/delivery/domain/DeliveryAddress.java
@@ -1,17 +1,25 @@
 package hello.delivery.delivery.domain;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 public class DeliveryAddress {
 
+    @NotBlank(message = "배달 주소는 필수 입력 값입니다.")
     private final String address;
 
     @Builder
     private DeliveryAddress(@JsonProperty("address") String address) {
         this.address = address;
+    }
+
+    public static DeliveryAddress of(String address) {
+        return DeliveryAddress.builder()
+                .address(address)
+                .build();
     }
 
 }
