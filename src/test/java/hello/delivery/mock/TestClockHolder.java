95a1daf36da7650060972628aa596fb116d3e989
wooseop <useop0821@gmail.com>
2025-11-23 04:38:24 +0900

    test: 고정된 시간으로 변경

--- src/test/java/hello/delivery/mock/TestClockHolder.java
+++ src/test/java/hello/delivery/mock/TestClockHolder.java
@@ -1,21 +1,20 @@
 package hello.delivery.mock;
 
 import hello.delivery.common.service.port.ClockHolder;
-import java.time.Clock;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 public class TestClockHolder implements ClockHolder {
 
-    private final Clock clock = Clock.systemUTC();
+    private final LocalDateTime fixedDateTime = LocalDateTime.of(2025, 11, 23, 03, 41, 0);
 
     @Override
     public LocalDate now() {
-        return LocalDate.now(clock);
+        return fixedDateTime.toLocalDate();
     }
 
     @Override
     public LocalDateTime nowDateTime() {
-        return LocalDateTime.now(clock);
+        return fixedDateTime;
     }
 }
