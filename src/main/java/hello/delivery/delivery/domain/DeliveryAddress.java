c6051e4d95e2f2e06970288bf7d08da75c7622f2
wooseop <useop0821@gmail.com>
2025-11-24 20:07:23 +0900

    feat: DeliveryAddress 클래스에 주소 필드 수정 및 Embeddable 어노테이션 추가

--- src/main/java/hello/delivery/delivery/domain/DeliveryAddress.java
+++ src/main/java/hello/delivery/delivery/domain/DeliveryAddress.java
@@ -1,15 +1,20 @@
 package hello.delivery.delivery.domain;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.Embeddable;
 import jakarta.validation.constraints.NotBlank;
+import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@Embeddable
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class DeliveryAddress {
 
     @NotBlank(message = "배달 주소는 필수 입력 값입니다.")
-    private final String address;
+    private String address;
 
     @Builder
     private DeliveryAddress(@JsonProperty("address") String address) {
